838ae5073737cab8dd9e23219a9351460941c73a
Likitha Khatri <likithakhatri5774@gmail.com>
2024-12-04 12:41:41 +0530

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,10 +2,9 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-// Protect middleware to authenticate users
 const protect = asyncHandler(async (req, res, next) => {
   try {
-    const token = req.cookies.token; // Read token from cookies
+    const token = req.cookies.token;
     if (!token) {
       res.status(401);
       throw new Error("Not authorized, please login");
@@ -13,40 +12,51 @@ const protect = asyncHandler(async (req, res, next) => {
 
     // Verify token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(verified.id).select("-password"); // Get user from token
 
+    // Get user id from token
+    const user = await User.findById(verified.id).select("-password");
     if (!user) {
       res.status(404);
       throw new Error("User not found");
     }
 
-    req.user = user; // Attach user to request
+    req.user = user;
     next();
   } catch (error) {
     res.status(401).json({ message: "Not authorized, please login" });
   }
 });
 
-// Middleware to restrict access to admins only
+const verifiedOnly = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.isVerified) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, account not verified");
+  }                                                                                                                                                                                                     
+});
+
+const authorOnly = asyncHandler(async (req, res, next) => {
+  if (req.user.role === "author" || req.user.role === "admin") {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an author");
+  }
+});
+
 const adminOnly = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     next();
   } else {
-    res.status(403).json({ message: "Not authorized as an admin" });
-  }
-});
-
-// Middleware to restrict access to authors or admins
-const authorOnly = asyncHandler(async (req, res, next) => {
-  if (req.user && (req.user.role === "author" || req.user.role === "admin")) {
-    next();
-  } else {
-    res.status(403).json({ message: "Not authorized as an author" });
+    res.status(401);
+    throw new Error("Not authorized as an admin");
   }
 });
 
 module.exports = {
   protect,
-  adminOnly,
+  verifiedOnly,
   authorOnly,
+  adminOnly,
 };
