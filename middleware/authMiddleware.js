aa768d617dae0c0662a415525b3b5d7bfec3a7a1
Likitha Khatri <likithakhatri5774@gmail.com>
2024-12-04 12:28:48 +0530

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,9 +2,10 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
+// Protect middleware to authenticate users
 const protect = asyncHandler(async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.token; // Read token from cookies
     if (!token) {
       res.status(401);
       throw new Error("Not authorized, please login");
@@ -12,56 +13,40 @@ const protect = asyncHandler(async (req, res, next) => {
 
     // Verify token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    // Get user id from token
-    const user = await User.findById(verified.id).select("-password");
+    const user = await User.findById(verified.id).select("-password"); // Get user from token
 
     if (!user) {
       res.status(404);
       throw new Error("User not found");
     }
-    if (user.role === "suspended") {
-      res.status(400);
-      throw new Error("User suspended, please contact support");
-    }
 
-    req.user = user;
+    req.user = user; // Attach user to request
     next();
   } catch (error) {
-    res.status(401);
-    throw new Error("Not authorized, please login");
+    res.status(401).json({ message: "Not authorized, please login" });
   }
 });
 
-const verifiedOnly = asyncHandler(async (req, res, next) => {
-  if (req.user && req.user.isVerified) {
-    next();
-  } else {
-    res.status(401);
-    throw new Error("Not authorized, account not verified");
-  }                                                                                                                                                                                                     
-});
-
-const authorOnly = asyncHandler(async (req, res, next) => {
-  if (req.user.role === "author" || req.user.role === "admin") {
-    next();
-  } else {
-    res.status(401);
-    throw new Error("Not authorized as an author");
-  }
-});
-
+// Middleware to restrict access to admins only
 const adminOnly = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     next();
   } else {
-    res.status(401);
-    throw new Error("Not authorized as an admin");
+    res.status(403).json({ message: "Not authorized as an admin" });
+  }
+});
+
+// Middleware to restrict access to authors or admins
+const authorOnly = asyncHandler(async (req, res, next) => {
+  if (req.user && (req.user.role === "author" || req.user.role === "admin")) {
+    next();
+  } else {
+    res.status(403).json({ message: "Not authorized as an author" });
   }
 });
 
 module.exports = {
   protect,
-  verifiedOnly,
-  authorOnly,
   adminOnly,
+  authorOnly,
 };
